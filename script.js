cccae69972ca063e6b85df830df4b559bcb7e9e7
CharlotEden <75186765+CharlotEden@users.noreply.github.com>
2022-05-30 16:02:52 -0400

    Added music yay
    piano, ambience, and jazz

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ let AudioLibrary = [waves, jbirds, rain1, night, runningwater, birdschirp, seagu
   wind, breakfast, cafe1, cafe2, fireplace, library, paper, rain2, storm, highway, river, mouse,
 ocean, pencil1,pencil2, storm2, clock, page, typewriter, typing, ]
 
+let jazz = [betonit, bread, soulandmind, nightfalling]
+
+let piano = [childhood, enchanted, eternallove, purpose, undertow, warmmemories]
+
+let ambience = [ethereal, filaments, fragments, jul]
+
+
+let MusicLibrary = []
+
+for (let i = 0; i < jazz.length; i++) {
+  const j = jazz[i];
+  MusicLibrary.push(j)
+}
+for (let o = 0; o < piano.length; o++) {
+  const p = piano[o];
+  MusicLibrary.push(p)
+}
+for (let e = 0; e < ambience.length; e++) {
+  const a = ambience[e];
+  MusicLibrary.push(a)
+}
+
+
 function randomTitle() {
     let r = Math.floor(Math.random() * bgsTitle.length)
     vid.src = "img/live/" + bgsTitle[r] + ".mp4"
@@ -57,5 +80,45 @@ function setAll() {
     sound.volume = 0.5
     sound.loop = true
   }
+  for (let i = 0; i < MusicLibrary.length; i++) {
+    const sound = MusicLibrary[i];
+    sound.volume = 0.5
+    sound.loop = true
+  }
 }
 setAll()
+
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+
+function toggleMusic (checkID, sound) {
+  for (let i = 0; i < MusicLibrary.length; i++) {
+    const x = MusicLibrary[i];
+    x.pause()
+  }
+  if(checkID.checked === true) {
+    sound.play()
+  }
+}
+
+function beginPlaylist() {
+
+}
