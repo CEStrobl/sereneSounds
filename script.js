4360dc7992fb06b33b058a4a2d6b0bfb1a2d873f
CharlotEden <75186765+CharlotEden@users.noreply.github.com>
2022-05-30 17:20:35 -0400

    auto bg

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 let vid = document.getElementById("myVideo")
+let vid2 = document.getElementById("myVideo3")
 let title = document.getElementById("title")
 let app = document.getElementById("app")
 
 let random = ""
 
-let bgsTitle = ["snowfall", "ocean", "clouds", "autumntrees",]
+let bgsTitle = ["snowfall", "ocean", "clouds", "autumntrees", "beach4", "beach2", "castle",
+"forest1", "forest3", "rain1", "rain3", "shop1", "trees3", "window1",
+]
+
+let snowfall = 0
+let oceanBG = 0
+let autumntrees = 0
+let beach4 = 0
+let castle = 0
+let rain1BG = 0
+let rain3 = 0
+let shop1 = 0
+let trees3 = 0
+let window1 = 0
+
+let enableAutoBG =  document.getElementById("autobg")
+
+
 
 let AudioLibrary = [waves, jbirds, rain1, night, runningwater, birdschirp, seagulls,
-  wind, breakfast, cafe1, cafe2, fireplace, library, paper, rain2, storm, highway, river, mouse,
+  wind, cafe1, cafe2, fireplace, library, paper, rain2, storm, highway, river, mouse,
 ocean, pencil1,pencil2, storm2, clock, page, typewriter, typing, ]
 
 let jazz = [betonit, bread, soulandmind, nightfalling]
@@ -15,6 +33,8 @@ let jazz = [betonit, bread, soulandmind, nightfalling]
 let piano = [childhood, enchanted, eternallove, purpose, undertow, warmmemories]
 
 let ambience = [ethereal, filaments, fragments, jul]
+
+
 
 
 let MusicLibrary = []
@@ -36,6 +56,7 @@ for (let e = 0; e < ambience.length; e++) {
 function randomTitle() {
     let r = Math.floor(Math.random() * bgsTitle.length)
     vid.src = "img/live/" + bgsTitle[r] + ".mp4"
+    vid2.src = "img/live/" + bgsTitle[r] + ".mp4"
     random = bgsTitle[r]
 }
 randomTitle()
@@ -62,8 +83,60 @@ function showElement(x) {
 function toggleSound (checkID, sound) {
   if(checkID.checked === true) {
     sound.play()
+    calcPoints(sound, 1)
   } else {
     sound.pause()
+    calcPoints(sound, -1)
+  }
+  if(enableAutoBG.checked === true) {
+    autoBackground()
+  }
+}
+
+function calcPoints(sound, x) {
+  if(sound === ocean || sound === seagulls){
+    beach4+=x
+  }
+  if (sound === wind) {
+    autumntrees+=x
+    snowfall+=x
+    trees3+=x
+    rain3+=x
+  }
+  if (sound === rain1 || sound === storm) {
+    rain1BG+=x
+    rain3+=x
+    window1+=x
+  }
+  if(sound === night){
+    snowfall+=x
+    window1+=x
+  }
+  if (sound === birdschirp) {
+    autumntrees+=x
+    trees3+=x
+  }
+  if (sound===highway) {
+    rain1+=x
+  }
+  if (sound===fireplace) {
+    castle+=x
+    window+=x
+  }
+
+  if (sound === pencil1 || sound === typewriter || sound === clock ||
+   sound === library ) {
+    window1+=x
+    castle+=x
+  }
+  if (sound === typing || sound === mouse ||
+    sound === cafe1 ) {
+    shop1+=x
+  }
+  if(sound === page ||sound === paper) {
+    window1+=x
+    castle+=x
+    shop1+=x
   }
 }
 
@@ -121,4 +194,65 @@ function toggleMusic (checkID, sound) {
 
 function beginPlaylist() {
 
+}
+
+
+function hideVid2(target) {
+  vid2.style.opacity = 0;
+  console.log("0 opacity")
+  setTimeout(() => {
+      vid2.src = "img/live/" + target + ".mp4"
+      console.log("switched hidden vid")
+    }, 600);
+    setTimeout(() => {
+      console.log("bring back opacity")
+      vid2.style.opacity = 1;
+    }, 1000);
+    setTimeout(() => {
+      vid.src = "img/live/" + target + ".mp4"
+      vid.currentTime=1.8
+      console.log("set underneath")
+    }, 1800);
+}
+
+
+function autoBackground() {
+  var num = [autumntrees, beach4, castle,
+  rain1BG, rain3, shop1, snowfall, trees3, window1,];
+  let max = Math.max.apply(null, num)
+
+  let x =max
+
+  if (x === autumntrees) {
+    hideVid2("autumntrees")
+  }
+  else if (x === beach4) {
+    hideVid2("beach4")
+  }
+  else if (x === castle) {
+    hideVid2("castle")
+  }
+  else if (x === rain1BG) {
+    hideVid2("rain1")
+  }
+  else if (x === rain3) {
+    hideVid2("rain1")
+  }
+  else if (x === snowfall) {
+    hideVid2("snowfall")
+  }
+  else if (x === trees3) {
+    hideVid2("trees3")
+  }
+  else if (x === window1) {
+    hideVid2("window1")
+  }
+  else if (x === shop1) {
+    hideVid2("shop1")
+  }
+    // for (let i = 0; i < num.length; i++) {
+    //   const x = num[i];
+    //   if(x === max){
+    //   }
+    // }
 }
